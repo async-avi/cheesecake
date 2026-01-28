9b66905d515e4f63a827bb45ef132ba875f50d9c
async-avi <work.abhinav@yahoo.com>
2026-01-28 21:40:41 +0530

    Working key press and hold

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import keyboard
 
-def on_press(key):
-    print(key)
+key_down = False
 
-keyboard.on_press(on_press)        
-keyboard.wait("space")
+def detectKeyPress():
+    global key_down
+    if not key_down:
+        key_down = True
+        print("hello boss")
+
+def detectKeyRelease():
+    global key_down
+    if key_down:
+        key_down = False
+        print("Gdbye boss")
+
+keyboard.on_press_key("space", lambda e: detectKeyPress())
+keyboard.on_release_key("space", lambda e: detectKeyRelease())
+
+keyboard.wait("esc")                                    
